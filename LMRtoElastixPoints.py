c651e5c4daca5c563e22a86b3253bd1d52eeabf7
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2023-06-07 13:48:55 +0200

    create source and target points simultaneously

--- LMRtoElastixPoints.py
+++ LMRtoElastixPoints.py
@@ -17,8 +17,21 @@ with open(mypath + filename) as file:
     openFile = json.load(file)
     
 xyz_list = [(i["px"], i["py"], i["pz"]) for i in openFile]
-    
+
 with open(mypath + age + '_points.pts', 'w') as f:
+    f.write("index")
+    f.write('\n')    
+    f.write(str(len(xyz_list)))
+    f.write('\n')
+    
+    for point in xyz_list:
+        
+        f.write(f"{round(point[0])} {round(point[1])} {round(point[2])}\n")
+        
+    
+xyz_list = [(i["x"], i["y"], i["z"]) for i in openFile]
+
+with open(mypath + 'adult_points.pts', 'w') as f:
     f.write("index")
     f.write('\n')    
     f.write(str(len(xyz_list)))
