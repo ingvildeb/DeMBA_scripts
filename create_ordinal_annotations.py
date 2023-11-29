fcd28849f2555b34d6c77b539a2a3aaa36603191
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2023-11-29 04:06:52 +0100

    woops

--- create_ordinal_annotations.py
+++ create_ordinal_annotations.py
@@ -9,26 +9,24 @@ from tqdm import tqdm
 import numpy as np
 import json
 
-base_path = r"Z:/HBP_Atlasing/Developmental_atlases/DeMBA_Developmental mouse brain atlas/DeMBA-v1/01_working-environment/01_Data/Volumes_for_elastix/"
-base_vol_path = r"Z:/HBP_Atlasing/Developmental_atlases/DeMBA_Developmental mouse brain atlas/DeMBA-v1/01_working-environment/01_Data/Volumes_warped/"
-
+base_path = r"Z:/HBP_Atlasing/Developmental_atlases/DeMBA_Developmental mouse brain atlas/DeMBA-v1/01_working-environment/01_Data/Volumes_for_elastix"
 years = [2017, 2022]
 
-for year in years[1:]:
-    vol_path = rf"{base_vol_path}/annotation_10_{year}_reoriented_warp_ordinal.nii.gz"
+for year in years:
+    vol_path = rf"{base_path}/annotation_10_{year}_reoriented.nii.gz"
     vol = nib.load(vol_path)
     vol_data = vol.get_fdata()
-
-    with open(rf"{base_path}/{year}_lookup.json", 'r') as in_json:
-        id_map = json.load(in_json)
+    ids = np.unique(vol_data)
+    ordinal_ids = np.arange(len(ids))
+    id_map = {int(i):int(oid) for i,oid in zip(ids, ordinal_ids)}
+    with open(rf"{base_path}/{year}_lookup.json", 'w') as out_json:
+        json.dump(id_map, out_json)
     
     out_volume = np.zeros(vol_data.shape)
     for i, oid in tqdm(id_map.items()):
-        out_volume[vol_data == oid] = i
+        out_volume[vol_data == i] = oid
         
     out_img = nib.Nifti1Image(out_volume, vol.affine, vol.header)
-    out_filename = rf"{base_vol_path}/annotation_10_{year}_reoriented_warp_original_id.nii.gz"
+    out_filename = rf"{base_path}/ordinal_annotation_10_{year}_reoriented.nii.gz"
     nib.save(out_img, out_filename)
         
-
-
