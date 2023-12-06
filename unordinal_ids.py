9990d4cd00c168307de04c04eadc32a5dc06cba3
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2023-12-06 09:20:33 +0100

    Update unordinal_ids.py
    Make sure the output IDs are integers

--- unordinal_ids.py
+++ unordinal_ids.py
@@ -10,25 +10,26 @@ import numpy as np
 import json
 
 base_path = r"Z:/HBP_Atlasing/Developmental_atlases/DeMBA_Developmental mouse brain atlas/DeMBA-v1/01_working-environment/01_Data/Volumes_for_elastix/"
-base_vol_path = r"Z:/HBP_Atlasing/Developmental_atlases/DeMBA_Developmental mouse brain atlas/DeMBA-v1/01_working-environment/01_Data/Volumes_warped/"
+base_vol_path = r"C:\Users\ingvieb\elastix_testing\ordinal_ids\P7\runs\run1/"
 
 years = [2017, 2022]
 
 for year in years[:]:
-    vol_path = rf"{base_vol_path}/annotation_10_{year}_reoriented_warp_ordinal.nii.gz"
+    vol_path = rf"{base_vol_path}/P7_resultSegmentation_{year}.nii"
     vol = nib.load(vol_path)
     vol_data = vol.get_fdata()
+    vol_data = vol_data.astype(int)
 
     with open(rf"{base_path}/{year}_lookup.json", 'r') as in_json:
         id_map = json.load(in_json)
     
-    out_volume = np.zeros(vol_data.shape)
+    out_volume = np.zeros(vol_data.shape).astype(int)
     for i, oid in tqdm(id_map.items()):
-        out_volume[vol_data == oid] = i
+        out_volume[vol_data == oid] = int(i)
         
     out_img = nib.Nifti1Image(out_volume, vol.affine, vol.header)
-    out_filename = rf"{base_vol_path}/annotation_10_{year}_reoriented_warp_original_id.nii.gz"
+    out_img.set_data_dtype(np.uint32)
+    out_filename = rf"{base_vol_path}/P7_resultSegmentation_allenID_{year}.nii.gz"
     nib.save(out_img, out_filename)
-        
-
+    
 
