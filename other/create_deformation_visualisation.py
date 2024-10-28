1947a6f6e2eafff3ff9d705de686bc6e6e99c851
polarbean <harry.carey95@gmail.com>
2024-10-28 15:56:18 +0100

    small fix

--- other/create_deformation_visualisation.py
+++ other/create_deformation_visualisation.py
@@ -2,8 +2,22 @@ import CCF_translator
 import nibabel as nib
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import math
 from tqdm import tqdm
+import inspect
+import os
+
+# Get the file path of the package
+package_path = inspect.getfile(CCF_translator)
+
+# Get the directory of the package
+package_dir = os.path.dirname(package_path)
+
+
+
+
+
 data_dir = r"/home/harryc/github/CCF_translator/CCF_translator/metadata/deformation_fields/demba_dev_mouse"
 key_ages = [56, 28, 21, 14, 7, 4]
 
@@ -53,25 +67,25 @@ slice_index = 600
 
 for i in tqdm(range(len(key_ages) - 1)):
     age = key_ages[ i + 1]
-    mag = key_ages[i] - age 
     template_vol_path = f"{template_dir}/DeMBA_P{age}_AllenSTPT_10um.nii.gz"
     template_img = nib.load(template_vol_path)
     template_arr = template_img.get_fdata()
-    metadata_path = r"/home/harryc/github/CCF_translator/CCF_translator/metadata/translation_metadata.csv"
+    base_path = r"/home/harryc/github/CCF_translator/CCF_translator/"
+    metadata_path = rf"{package_dir}/metadata/translation_metadata.csv"
     metadata = pd.read_csv(metadata_path)
     G = CCF_translator.deformation.route_calculation.create_G(metadata)
     source = f"demba_dev_mouse_P{age}"
     target = "demba_dev_mouse_P56"
     route = CCF_translator.deformation.route_calculation.calculate_route(source, target, G)
     deform_arr, pad_sum, flip_sum, dim_order_sum, final_voxel_size = (
-        apply_deformation.combine_route(
-            route, self.voxel_size_micron, base_path, self.metadata
+        CCF_translator.deformation.apply_deformation.combine_route(
+            route, 20, base_path, metadata
         )
     )
     ###############################
     # Extract the deformation vectors for the 2D slice
-    deformation_x = deform_arr[slice_index // 2, :, :, 1]
-    deformation_y = deform_arr[slice_index // 2, :, :, 2]
+    deformation_x = deform_arr[1, slice_index // 2, :, :].copy()
+    deformation_y = deform_arr[2, slice_index // 2, :, :].copy()
     deformation_x *= 2
     deformation_y *= 2
 
@@ -79,10 +93,10 @@ for i in tqdm(range(len(key_ages) - 1)):
     grid_spacing = 62.52215  # Adjust the spacing as needed
     x, y = np.meshgrid(np.linspace(0, dim2, int(dim2 / grid_spacing) + 1), 
                     np.linspace(0, dim1, int(dim1 / grid_spacing) + 1))
-    grid_spacing = np.floor(grid_spacing / 2 ).astype(int)
+    grid_spacing = np.ceil(grid_spacing / 2 ).astype(int)
     # Apply the deformation to the grid points
-    deformed_x =( x - deformation_x[::grid_spacing, ::grid_spacing ]) 
-    deformed_y =( y - deformation_y[::grid_spacing , ::grid_spacing ])
+    deformed_x =( x + deformation_x[::grid_spacing, ::grid_spacing ]) 
+    deformed_y =( y + deformation_y[::grid_spacing , ::grid_spacing ])
     
     # Plot the image
     plt.imshow(template_arr[600, :, :], cmap='gray')
