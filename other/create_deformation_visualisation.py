0b7404c33873c7914962fb69d4f72da896d70f21
polarbean <harry.carey95@gmail.com>
2024-10-29 11:16:05 +0100

    plotting deformation

--- other/create_deformation_visualisation.py
+++ other/create_deformation_visualisation.py
@@ -7,6 +7,8 @@ import math
 from tqdm import tqdm
 import inspect
 import os
+from scipy.interpolate import griddata
+from scipy.ndimage import generic_filter
 
 # Get the file path of the package
 package_path = inspect.getfile(CCF_translator)
@@ -23,9 +25,21 @@ key_ages = [56, 28, 21, 14, 7, 4]
 
 
 
-
-
-
+def interpolate_nans(data):
+    x, y = np.indices(data.shape)
+    valid_mask = ~np.isnan(data)
+    coords = np.array([x[valid_mask], y[valid_mask]]).T
+    values = data[valid_mask]
+    
+    # Initial linear interpolation
+    interpolated = griddata(coords, values, (x, y), method='linear')
+    
+    # Handle any remaining NaNs with nearest-neighbor interpolation
+    remaining_nans = np.isnan(interpolated)
+    if np.any(remaining_nans):
+        interpolated[remaining_nans] = griddata(coords, values, (x, y), method='nearest')[remaining_nans]
+    
+    return interpolated
 
 age = key_ages[0]
 
@@ -66,26 +80,35 @@ plt.show()
 slice_index = 600
 
 for i in tqdm(range(len(key_ages) - 1)):
+    above_age = key_ages[i]
     age = key_ages[ i + 1]
+    mag = key_ages[i] - age
     template_vol_path = f"{template_dir}/DeMBA_P{age}_AllenSTPT_10um.nii.gz"
     template_img = nib.load(template_vol_path)
     template_arr = template_img.get_fdata()
     base_path = r"/home/harryc/github/CCF_translator/CCF_translator/"
     metadata_path = rf"{package_dir}/metadata/translation_metadata.csv"
     metadata = pd.read_csv(metadata_path)
-    G = CCF_translator.deformation.route_calculation.create_G(metadata)
     source = f"demba_dev_mouse_P{age}"
     target = "demba_dev_mouse_P56"
+
+    G = CCF_translator.deformation.route_calculation.create_G(metadata)
+
     route = CCF_translator.deformation.route_calculation.calculate_route(source, target, G)
-    deform_arr, pad_sum, flip_sum, dim_order_sum, final_voxel_size = (
-        CCF_translator.deformation.apply_deformation.combine_route(
-            route, 20, base_path, metadata
-        )
-    )
+
+    # deform_arr, pad_sum, flip_sum, dim_order_sum, final_voxel_size = (
+    #     CCF_translator.deformation.apply_deformation.combine_route(
+    #         route, 20, base_path, metadata
+    #     )
+    # )
+
+    deform_arr = nib.load(f"{base_path}/metadata/deformation_fields/demba_dev_mouse/{above_age}_pull_{above_age - 1}.nii.gz").get_fdata()
+    deform_arr *= mag
     ###############################
     # Extract the deformation vectors for the 2D slice
-    deformation_x = deform_arr[1, slice_index // 2, :, :].copy()
-    deformation_y = deform_arr[2, slice_index // 2, :, :].copy()
+    deformation_x = deform_arr[slice_index // 2, :, :, 1].copy()
+    deformation_y = deform_arr[slice_index // 2, :, :, 2].copy()
+    
     deformation_x *= 2
     deformation_y *= 2
 
@@ -93,24 +116,39 @@ for i in tqdm(range(len(key_ages) - 1)):
     grid_spacing = 62.52215  # Adjust the spacing as needed
     x, y = np.meshgrid(np.linspace(0, dim2, int(dim2 / grid_spacing) + 1), 
                     np.linspace(0, dim1, int(dim1 / grid_spacing) + 1))
-    grid_spacing = np.ceil(grid_spacing / 2 ).astype(int)
+                    
+    grid_spacing = np.round(grid_spacing / 2 ).astype(int)
     # Apply the deformation to the grid points
-    deformed_x =( x + deformation_x[::grid_spacing, ::grid_spacing ]) 
-    deformed_y =( y + deformation_y[::grid_spacing , ::grid_spacing ])
+    dx = deformation_x[::grid_spacing, ::grid_spacing ]
+    dy = deformation_y[::grid_spacing , ::grid_spacing ]
+
+    # Interpolate the NaN values
+    if np.isnan(dx).any():
+        dx = interpolate_nans(dx)
+    if np.isnan(dy).any():
+        dy = interpolate_nans(dy)
+    deformed_x = x + dx
+    deformed_y = y + dy
+    
+    plt.figure(figsize=(10, 10), dpi=300)
     
     # Plot the image
-    plt.imshow(template_arr[600, :, :], cmap='gray')
+    fig, ax = plt.subplots()
+    fig.patch.set_facecolor('black')
+    ax.set_facecolor('black')
+    ax.imshow(template_arr[600, :, :], cmap='gray')
 
     # Plot the deformed grid
     for i in range(y.shape[0]):
-        plt.plot(deformed_x[i, :], deformed_y[i, :], color='red', linewidth=1)
+        ax.plot(deformed_x[i, :], deformed_y[i, :], color='red', linewidth=1)
     for j in range(x.shape[1]):
-        plt.plot(deformed_x[:, j], deformed_y[:, j], color='red', linewidth=1)
+        ax.plot(deformed_x[:, j], deformed_y[:, j], color='red', linewidth=1)
 
     # plt.title("Deformation Field Visualization")
-    plt.axis('off')
+    ax.axis('off')
     plt.tight_layout()
+    # Create a figure with a higher resolution
 
     # Save the plot as an SVG file with higher resolution
-    plt.savefig(f"../data_files/deformation_field_P{age}.svg", format="svg", dpi=300)
+    plt.savefig(f"../data_files/deformation_field_P{age}.svg", format="svg", dpi=300, facecolor=fig.get_facecolor())
     plt.show()
