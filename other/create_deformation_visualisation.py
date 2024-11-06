3c3b9f8db6b375c5d6f69e4d7082c756cd8b8d60
polarbean <harry.carey95@gmail.com>
2024-11-06 11:02:52 +0100

    update deformation

--- other/create_deformation_visualisation.py
+++ other/create_deformation_visualisation.py
@@ -17,6 +17,9 @@ package_path = inspect.getfile(CCF_translator)
 package_dir = os.path.dirname(package_path)
 
 
+
+
+
 data_dir = r"/home/harryc/github/CCF_translator/CCF_translator/metadata/deformation_fields/demba_dev_mouse"
 key_ages = [56, 28, 21, 14, 7, 4]
 
@@ -38,151 +41,7 @@ def interpolate_nans(data):
     
     return interpolated
 
->>>>>>> 5d97297675c22f964162e042faed0a4953833a34
 age = key_ages[0]
-
-
-template_dir = r"/home/harryc/github/DeMBA_scripts/data_files/allen_stpt_10um/"
-template_vol_path = f"{template_dir}/DeMBA_P{age}_AllenSTPT_10um.nii.gz"
-template_img = nib.load(template_vol_path)
-template_arr = template_img.get_fdata()
-dim1 = template_arr.shape[1]
-dim2 = template_arr.shape[2]
-
-# Create a regular grid
-grid_spacing = 61.42215  # Adjust the spacing as needed
-
-x, y = np.meshgrid(
-    np.linspace(0, dim2, int(dim2 / grid_spacing) + 1),
-    np.linspace(0, dim1, int(dim1 / grid_spacing) + 1),
-)
-
-# Create a figure with a higher resolution
-plt.figure(figsize=(10, 10), dpi=300)
-
-# Plot the image
-plt.imshow(template_arr[600, :, :], cmap="gray")
-
-# Plot the deformed grid
-for i in range(y.shape[0]):
-    plt.plot(x[i, :], y[i, :], color="red", linewidth=1)
-for j in range(x.shape[1]):
-    plt.plot(x[:, j], y[:, j], color="red", linewidth=1)
-
-# plt.title("Deformation Field Visualization")
-plt.axis("off")
-plt.tight_layout()
-
-# Save the plot as an SVG file with higher resolution
-plt.savefig(f"../data_files/deformation_field_P{age}.svg", format="svg", dpi=300)
-
-plt.show()
-slice_index = 600
-
-for i in tqdm(range(len(key_ages) - 1)):
-<<<<<<< HEAD
-    age = key_ages[i + 1]
-=======
-    above_age = key_ages[i]
-    age = key_ages[ i + 1]
-    mag = key_ages[i] - age
->>>>>>> 5d97297675c22f964162e042faed0a4953833a34
-    template_vol_path = f"{template_dir}/DeMBA_P{age}_AllenSTPT_10um.nii.gz"
-    template_img = nib.load(template_vol_path)
-    template_arr = template_img.get_fdata()
-    base_path = r"/home/harryc/github/CCF_translator/CCF_translator/"
-    metadata_path = rf"{package_dir}/metadata/translation_metadata.csv"
-    metadata = pd.read_csv(metadata_path)
-    source = f"demba_dev_mouse_P{age}"
-    target = "demba_dev_mouse_P56"
-<<<<<<< HEAD
-    route = CCF_translator.deformation.route_calculation.calculate_route(
-        source, target, G
-    )
-    deform_arr, pad_sum, flip_sum, dim_order_sum, final_voxel_size = (
-        CCF_translator.deformation.apply_deformation.combine_route(
-            route, 20, base_path, metadata
-        )
-    )
-=======
-    deform_arr = nib.load(f"{base_path}/metadata/deformation_fields/demba_dev_mouse/{above_age}_pull_{above_age - 1}.nii.gz").get_fdata()
-    deform_arr *= mag
->>>>>>> 5d97297675c22f964162e042faed0a4953833a34
-    ###############################
-    # Extract the deformation vectors for the 2D slice
-    deformation_x = deform_arr[slice_index // 2, :, :, 1].copy()
-    deformation_y = deform_arr[slice_index // 2, :, :, 2].copy()
-    
-    deformation_x *= -1
-    deformation_y *= -1
-
-    # Create a regular grid
-    grid_spacing = 62.52215  # Adjust the spacing as needed
-<<<<<<< HEAD
-    x, y = np.meshgrid(
-        np.linspace(0, dim2, int(dim2 / grid_spacing) + 1),
-        np.linspace(0, dim1, int(dim1 / grid_spacing) + 1),
-    )
-    grid_spacing = np.ceil(grid_spacing / 2).astype(int)
-    # Apply the deformation to the grid points
-    deformed_x = x + deformation_x[::grid_spacing, ::grid_spacing]
-    deformed_y = y + deformation_y[::grid_spacing, ::grid_spacing]
-
-    # Plot the image
-    plt.imshow(template_arr[600, :, :], cmap="gray")
-
-    # Plot the deformed grid
-    for i in range(y.shape[0]):
-        plt.plot(deformed_x[i, :], deformed_y[i, :], color="red", linewidth=1)
-    for j in range(x.shape[1]):
-        plt.plot(deformed_x[:, j], deformed_y[:, j], color="red", linewidth=1)
-
-    # plt.title("Deformation Field Visualization")
-    plt.axis("off")
-=======
-    x, y = np.meshgrid(np.linspace(0, dim2, int(dim2 / grid_spacing) + 1), 
-                    np.linspace(0, dim1, int(dim1 / grid_spacing) + 1))
-                    
-    grid_spacing = np.round(grid_spacing / 2 ).astype(int)
-    # Apply the deformation to the grid points
-    dx = deformation_x[::grid_spacing, ::grid_spacing ]
-    dy = deformation_y[::grid_spacing , ::grid_spacing ]
-
-    # Interpolate the NaN values
-    if np.isnan(dx).any():
-        dx = interpolate_nans(dx)
-    if np.isnan(dy).any():
-        dy = interpolate_nans(dy)
-    deformed_x = x + dx
-    deformed_y = y + dy
-    
-    plt.figure(figsize=(10, 10), dpi=300)
-    
-    # Plot the image
-    fig, ax = plt.subplots()
-    fig.patch.set_facecolor('black')
-    ax.set_facecolor('black')
-    ax.imshow(template_arr[600, :, :], cmap='gray')
-
-    # Plot the deformed grid
-    for i in range(y.shape[0]):
-        ax.plot(deformed_x[i, :], deformed_y[i, :], color='red', linewidth=1)
-    for j in range(x.shape[1]):
-        ax.plot(deformed_x[:, j], deformed_y[:, j], color='red', linewidth=1)
-
-    # plt.title("Deformation Field Visualization")
-    ax.axis('off')
->>>>>>> 5d97297675c22f964162e042faed0a4953833a34
-    plt.tight_layout()
-    # Create a figure with a higher resolution
-
-    # Save the plot as an SVG file with higher resolution
-    plt.savefig(f"../data_files/deformation_field_P{age}.svg", format="svg", dpi=300, facecolor=fig.get_facecolor())
-    plt.show()
-
-
-
-age = key_ages[-1]
 
 
 template_dir = r"/home/harryc/github/DeMBA_scripts/data_files/allen_stpt_10um/"
@@ -215,9 +74,10 @@ plt.axis('off')
 plt.tight_layout()
 
 # Save the plot as an SVG file with higher resolution
-plt.savefig(f"../data_files/reverse_deformation_field_P{age}.svg", format="svg", dpi=300)
+plt.savefig(f"../data_files/deformation_field_P{age}.svg", format="svg", dpi=300)
 
 plt.show()
+slice_index = 600
 
 for i in tqdm(range(len(key_ages) - 1)):
     above_age = key_ages[i]
