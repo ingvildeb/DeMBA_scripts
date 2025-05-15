d8ebacc17dc39a33709b3d7c430b8877b7a08db4
polarbean <harry.carey95@gmail.com>
2025-05-15 20:09:59 +0200

    add data files

--- 4_accuracy_analysis/1_calculate_per_point_accuracy.py
+++ 4_accuracy_analysis/1_calculate_per_point_accuracy.py
@@ -14,14 +14,14 @@ points_dataset_rat1 = pd.read_excel(
 points_dataset_rat2 = pd.read_excel(r"../data_files/DeMBA_landmarksValidation_Rater_2.xlsx")
 points_dataset_rat4 = pd.read_excel(
     r"../data_files/DeMBA_landmarksValidation_Rater_3.xlsx"
-)  # Harry's data
+)  # rater_4's data
 
-keep_names = points_dataset_r["Full name"][points_dataset_r["Remove"] != 1]
+keep_names = points_dataset_rat4["Full name"][points_dataset_rat4["Remove"] != 1]
 
-points_dataset_h = points_dataset_h[points_dataset_h["Full name"].isin(keep_names)]
-points_dataset_i = points_dataset_i[points_dataset_i["Full name"].isin(keep_names)]
-points_dataset_s = points_dataset_s[points_dataset_s["Full name"].isin(keep_names)]
-points_dataset_r = points_dataset_r[points_dataset_r["Full name"].isin(keep_names)]
+points_dataset_rat3 = points_dataset_rat3[points_dataset_rat3["Full name"].isin(keep_names)]
+points_dataset_rat1 = points_dataset_rat1[points_dataset_rat1["Full name"].isin(keep_names)]
+points_dataset_rat2 = points_dataset_rat2[points_dataset_rat2["Full name"].isin(keep_names)]
+points_dataset_rat4 = points_dataset_rat4[points_dataset_rat4["Full name"].isin(keep_names)]
 
 
 target_space = "demba_dev_mouse"
@@ -50,7 +50,7 @@ def one_vs_rest(one_point, rest):
     return abs(np.linalg.norm(rest_point - one_point))
 
 
-mean_older_points = extract_points_from_df(points_dataset_h, "adult")
+mean_older_points = extract_points_from_df(points_dataset_rat3, "adult")
 for m in ["iterative"]:  # , "individual"]:
     for i in tqdm(range(len(key_ages) - 1)):
 
@@ -59,42 +59,42 @@ for m in ["iterative"]:  # , "individual"]:
         younger_age = key_ages[i + 1]
 
         name_younger_age = f"P{str(younger_age).zfill(2)}"
-        younger_points_heidi = extract_points_from_df(
-            points_dataset_h, name_younger_age
+        younger_points_rater_3 = extract_points_from_df(
+            points_dataset_rat3, name_younger_age
         )
-        younger_points_ingvild = extract_points_from_df(
-            points_dataset_i, name_younger_age
+        younger_points_rater_1 = extract_points_from_df(
+            points_dataset_rat1, name_younger_age
         )
-        younger_points_simon = extract_points_from_df(
-            points_dataset_s, name_younger_age
+        younger_points_rater_2 = extract_points_from_df(
+            points_dataset_rat2, name_younger_age
         )
-        younger_points_harry = extract_points_from_df(
-            points_dataset_r, name_younger_age
-        )  # Extract younger points for Harry
+        younger_points_rater_4 = extract_points_from_df(
+            points_dataset_rat4, name_younger_age
+        )  # Extract younger points for rater_4
         # If any of the raters have placed the point outside the brain remove that point from the analysis
         top_mask_x = (
-            (younger_points_harry[:, 0] < 570)
-            & (younger_points_ingvild[:, 0] < 570)
-            & (younger_points_simon[:, 0] < 570)
-            & (younger_points_heidi[:, 0] < 570)
+            (younger_points_rater_4[:, 0] < 570)
+            & (younger_points_rater_1[:, 0] < 570)
+            & (younger_points_rater_2[:, 0] < 570)
+            & (younger_points_rater_3[:, 0] < 570)
         )
         top_mask_y = (
-            (younger_points_harry[:, 1] < 400)
-            & (younger_points_ingvild[:, 1] < 400)
-            & (younger_points_simon[:, 1] < 400)
-            & (younger_points_heidi[:, 1] < 400)
+            (younger_points_rater_4[:, 1] < 400)
+            & (younger_points_rater_1[:, 1] < 400)
+            & (younger_points_rater_2[:, 1] < 400)
+            & (younger_points_rater_3[:, 1] < 400)
         )
         top_mask_z = (
-            (younger_points_harry[:, 2] < 705)
-            & (younger_points_ingvild[:, 2] < 705)
-            & (younger_points_simon[:, 2] < 705)
-            & (younger_points_heidi[:, 2] < 705)
+            (younger_points_rater_4[:, 2] < 705)
+            & (younger_points_rater_1[:, 2] < 705)
+            & (younger_points_rater_2[:, 2] < 705)
+            & (younger_points_rater_3[:, 2] < 705)
         )
         bottom_mask = (
-            (younger_points_harry > 0)
-            & (younger_points_ingvild > 0)
-            & (younger_points_simon > 0)
-            & (younger_points_heidi > 0)
+            (younger_points_rater_4 > 0)
+            & (younger_points_rater_1 > 0)
+            & (younger_points_rater_2 > 0)
+            & (younger_points_rater_3 > 0)
         )
         mask = np.array((top_mask_x, top_mask_y, top_mask_z)).T & bottom_mask
 
@@ -172,18 +172,18 @@ for m in ["iterative"]:  # , "individual"]:
                 continue
             pred_point = points.values[i]
             new_mean_older_points.append(pred_point)
-            heidi_point = younger_points_heidi[i]
-            ingvild_point = younger_points_ingvild[i]
-            simon_point = younger_points_simon[i]
-            harry_point = younger_points_harry[i]
+            rater_3_point = younger_points_rater_3[i]
+            rater_1_point = younger_points_rater_1[i]
+            rater_2_point = younger_points_rater_2[i]
+            rater_4_point = younger_points_rater_4[i]
 
-            h_dist = one_vs_rest(heidi_point, [simon_point, ingvild_point, harry_point])
-            s_dist = one_vs_rest(simon_point, [heidi_point, ingvild_point, harry_point])
-            i_dist = one_vs_rest(ingvild_point, [simon_point, heidi_point, harry_point])
-            r_dist = one_vs_rest(harry_point, [simon_point, heidi_point, ingvild_point])
-            d1_dist = one_vs_rest(pred_point, [simon_point, ingvild_point, harry_point])
-            d2_dist = one_vs_rest(pred_point, [heidi_point, ingvild_point, harry_point])
-            d3_dist = one_vs_rest(pred_point, [heidi_point, simon_point, harry_point])
+            h_dist = one_vs_rest(rater_3_point, [rater_2_point, rater_1_point, rater_4_point])
+            s_dist = one_vs_rest(rater_2_point, [rater_3_point, rater_1_point, rater_4_point])
+            i_dist = one_vs_rest(rater_1_point, [rater_2_point, rater_3_point, rater_4_point])
+            r_dist = one_vs_rest(rater_4_point, [rater_2_point, rater_3_point, rater_1_point])
+            d1_dist = one_vs_rest(pred_point, [rater_2_point, rater_1_point, rater_4_point])
+            d2_dist = one_vs_rest(pred_point, [rater_3_point, rater_1_point, rater_4_point])
+            d3_dist = one_vs_rest(pred_point, [rater_3_point, rater_2_point, rater_4_point])
             # i want to make sure im comparing averages formed from only 2 individiuals
             d_dist = np.mean([d1_dist, d3_dist, d3_dist])
 
@@ -195,61 +195,61 @@ for m in ["iterative"]:  # , "individual"]:
 
             # Append coordinates
 
-            hx.append(heidi_point[0])
-            hy.append(heidi_point[1])
-            hz.append(heidi_point[2])
-            ix.append(ingvild_point[0])
-            iy.append(ingvild_point[1])
-            iz.append(ingvild_point[2])
+            hx.append(rater_3_point[0])
+            hy.append(rater_3_point[1])
+            hz.append(rater_3_point[2])
+            ix.append(rater_1_point[0])
+            iy.append(rater_1_point[1])
+            iz.append(rater_1_point[2])
             dx.append(pred_point[0])
             dy.append(pred_point[1])
             dz.append(pred_point[2])
-            sx.append(simon_point[0])
-            sy.append(simon_point[1])
-            sz.append(simon_point[2])
-            rx.append(harry_point[0])
-            ry.append(harry_point[1])
-            rz.append(harry_point[2])
+            sx.append(rater_2_point[0])
+            sy.append(rater_2_point[1])
+            sz.append(rater_2_point[2])
+            rx.append(rater_4_point[0])
+            ry.append(rater_4_point[1])
+            rz.append(rater_4_point[2])
         output_data = {
-            "Acronym": points_dataset_h["Acronym"],
-            "Full Name": points_dataset_h["Full name"],
-            "Distance Ingvild to others": ing,
-            "Distance Heidi to others": hei,
-            "Distance Simon to others": sim,
+            "Acronym": points_dataset_rat3["Acronym"],
+            "Full Name": points_dataset_rat3["Full name"],
+            "Distance rater_1 to others": ing,
+            "Distance rater_3 to others": hei,
+            "Distance rater_2 to others": sim,
             "Distance DeMBA to others": dem,
-            "Distance Harry to others": har,
-            "Heidi x": hx,
-            "Heidi y": hy,
-            "Heidi z": hz,
-            "Ingvild x": ix,
-            "Ingvild y": iy,
-            "Ingvild z": iz,
+            "Distance rater_4 to others": har,
+            "rater_3 x": hx,
+            "rater_3 y": hy,
+            "rater_3 z": hz,
+            "rater_1 x": ix,
+            "rater_1 y": iy,
+            "rater_1 z": iz,
             "Demba x": dx,
             "Demba y": dy,
             "Demba z": dz,
-            "Simon x": sx,
-            "Simon y": sy,
-            "Simon z": sz,
-            "Harry x": rx,
-            "Harry y": ry,
-            "Harry z": rz,
+            "rater_2 x": sx,
+            "rater_2 y": sy,
+            "rater_2 z": sz,
+            "rater_4 x": rx,
+            "rater_4 y": ry,
+            "rater_4 z": rz,
         }
         pd.DataFrame(output_data).to_csv(f"../data_files/{m}_{name_younger_age}.csv")
         mean_older_points = np.array(new_mean_older_points)
 
-        print(f"ingvild distance median: {np.nanmedian(ing)}")
-        print(f"Simon distance median: {np.nanmedian(sim)}")
-        print(f"Heidi distance median: {np.nanmedian(hei)}")
+        print(f"rater_1 distance median: {np.nanmedian(ing)}")
+        print(f"rater_2 distance median: {np.nanmedian(sim)}")
+        print(f"rater_3 distance median: {np.nanmedian(hei)}")
         print(f"DeMBA distance median: {np.nanmedian(dem)}")
-        print(f"Harry distance median: {np.nanmedian(har)}")
-        print(f"ingvild distance mean: {np.nanmean(ing)}")
-        print(f"Simon distance mean: {np.nanmean(sim)}")
-        print(f"Heidi distance mean: {np.nanmean(hei)}")
+        print(f"rater_4 distance median: {np.nanmedian(har)}")
+        print(f"rater_1 distance mean: {np.nanmean(ing)}")
+        print(f"rater_2 distance mean: {np.nanmean(sim)}")
+        print(f"rater_3 distance mean: {np.nanmean(hei)}")
         print(f"DeMBA distance mean: {np.nanmean(dem)}")
-        print(f"Harry distance mean: {np.nanmean(har)}")
+        print(f"rater_4 distance mean: {np.nanmean(har)}")
 
-        print(f"ingvild distance max: {np.nanmax(ing)}")
-        print(f"Simon distance max: {np.nanmax(sim)}")
-        print(f"Heidi distance max: {np.nanmax(hei)}")
+        print(f"rater_1 distance max: {np.nanmax(ing)}")
+        print(f"rater_2 distance max: {np.nanmax(sim)}")
+        print(f"rater_3 distance max: {np.nanmax(hei)}")
         print(f"DeMBA distance max: {np.nanmax(dem)}")
-        print(f"Harry distance max: {np.nanmax(har)}")
+        print(f"rater_4 distance max: {np.nanmax(har)}")
